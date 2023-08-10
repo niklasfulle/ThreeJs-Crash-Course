1d195a7c98b2930a81bf358b407781378e3a5cf4
Niklas Fulle <36989748+Salkin263@users.noreply.github.com>
2023-08-10 02:06:52 +0200

    added shaders, rotaion and tilt for earth

--- Seconde Project/main.js
+++ Seconde Project/main.js
@@ -1,4 +1,8 @@
 import * as THREE from "three";
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
+import atmosphereFragmentShader from "./shaders/atmospehreFragment.glsl";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
@@ -8,9 +12,45 @@ renderer.setSize(innerWidth, innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
+// create a sphere
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(10, 100, 100),
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: { value: new THREE.TextureLoader().load("./img/globe.jpg") },
+    },
+  })
+);
+
+sphere.rotation.set(0, 0, (-Math.PI * 23) / 180);
+
+scene.add(sphere);
+
+// create a atmosphere
+
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(10, 100, 100),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide,
+  })
+);
+
+atmosphere.scale.set(1.1, 1.1, 1.1);
+
+scene.add(atmosphere);
+
+camera.position.z = 30;
+
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  sphere.rotateY(0.001);
 }
 
 animate();
